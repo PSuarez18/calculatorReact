5db4f65d3dc9090a4de602888df087af42ec3843
ptsuarez91@hotmail.com <ptsuarez91@hotmail.com>
2023-10-27 21:53:19 -0300

    faltan descubrir bugs

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,34 @@
-import { useState } from 'react'
-import './App.css'
-import { Icon } from '@iconify/react'
+import { useState } from 'react';
+import './App.css';
+import { Icon } from '@iconify/react';
 
 function App() {
-  const [currentValue, setCurrentValue] = useState('0')
-  const [prevValue, setPrevValue] = useState<string | null>(null)
-  const [operator, setOperator] = useState<string | null>(null)
+  const [currentValue, setCurrentValue] = useState('0');
+  const [prevValue, setPrevValue] = useState<string | null>(null);
+  const [operator, setOperator] = useState<string | null>(null);
 
-
-  //Handler for Click the Numbers/ Manejador de Clicks en Numeros
   const numberButtonClick = (valueNumber: string) => {
-    if (currentValue === '0' || currentValue === 'Error') {
-      setCurrentValue(valueNumber)
+    if (currentValue === '0' || currentValue === 'Error' || currentValue === '') {
+      setCurrentValue(valueNumber);
     } else {
-      setCurrentValue(currentValue + valueNumber)
+      setCurrentValue(currentValue + valueNumber);
     }
   }
 
-  //Handler for Click the operator math/ Manejador de Click en Operaador Matematico
   const operatorButtonClick = (op: string) => {
     if (prevValue === null) {
-      setPrevValue(currentValue)
-      setCurrentValue('0')
-      setOperator(op)
-    }
-    else {
-      calculateResult()
-      setOperator(op)
-      setCurrentValue('0')
+      setPrevValue(currentValue);
+      setOperator(op);
+      setCurrentValue('0');
+    } else if (prevValue && operator && currentValue) {
+      calculateResult();
     }
   }
- 
 
-  //handler Result Math // Manejar de resultado matematico
   const calculateResult = () => {
     if (prevValue !== null && operator !== null) {
-      const num1 = parseFloat(prevValue)
-      const num2 = parseFloat(currentValue)
+      const num1 = parseFloat(prevValue);
+      const num2 = parseFloat(currentValue);
 
       switch (operator) {
         case '+':
@@ -55,37 +47,31 @@ function App() {
             setCurrentValue('Error');
           }
           break;
-        case "%":
-          {
-            setCurrentValue(((num1 * num2)  / 100).toString())
-          }
+        case '%':
+          setCurrentValue(((num1 * num2) / 100).toString());
           break;
         default:
           break;
       }
-
       setPrevValue(null);
       setOperator(null);
     }
   }
 
-  //Handler Result Button Equal // Manejador de Resultado Boton Igualdad
   const handleEqualsClick = () => {
     calculateResult();
+    setOperator(null);
   }
-
 
   const handlerClear = () => {
-    setCurrentValue('0')
-    setPrevValue(null)
-    setOperator(null)
+    setCurrentValue('0');
+    setPrevValue(null);
+    setOperator(null);
   }
 
-
-  //Handler Keys Number // Manejador Teclado Numerico
-  const handleKeyDown = (event:any) => {
+  const handleKeyDown = (event: any) => {
     const key = event.key;
-  
+
     if (/^[0-9]$/.test(key)) {
       numberButtonClick(key);
     } else if (key === '+' || key === '-' || key === '*' || key === '/') {
@@ -102,57 +88,87 @@ function App() {
       }
     }
   };
-  
-
-
 
   return (
-    <>
-      <div>
+    <div className="calculator-container">
+      <div className="calculator">
         <div>
-          <input value={currentValue} readOnly onKeyDown={handleKeyDown}></input>
-        </div>
-        <div>
-          <button onClick={() => operatorButtonClick('+')}>
-            <Icon icon="whh:calcplus" height={45} /></button>
-          <button onClick={() => operatorButtonClick('-')}>
-            <Icon icon="whh:calcminus" height={45} /></button>
-          <button onClick={() => operatorButtonClick('*')}>
-            <Icon icon="whh:calcmultiply" height={45} /></button>
-          <button onClick={() => operatorButtonClick('/')}>
-            <Icon icon="whh:calcdivide" height={45} /></button>
-          <button onClick={() => handleEqualsClick()}><Icon icon="whh:calcequals" height={45} /></button>
-          <button onClick={() => operatorButtonClick('%')}>
-            <Icon icon="fa-solid:percentage" height={45} /></button>
-          <button onClick={() => numberButtonClick('9')}>
-            <Icon icon="material-symbols:counter-9" height={45} /></button>
-          <button onClick={() => numberButtonClick('8')}>
-            <Icon icon="material-symbols:counter-8" height={45} /></button>
-          <button onClick={() => numberButtonClick('7')}>
-            <Icon icon="material-symbols:counter-7" height={45} /></button>
-          <button onClick={() => numberButtonClick('6')}>
-            <Icon icon="material-symbols:counter-6" height={45} /></button>
-          <button onClick={() => numberButtonClick('5')}>
-            <Icon icon="material-symbols:counter-5" height={45} /></button>
-          <button onClick={() => numberButtonClick('4')}>
-            <Icon icon="material-symbols:counter-4" height={45} /></button>
-          <button onClick={() => numberButtonClick('3')}>
-            <Icon icon="material-symbols:counter-3" height={45} /></button>
-          <button onClick={() => numberButtonClick('2')}>
-            <Icon icon="material-symbols:counter-2" height={45} /></button>
-          <button onClick={() => numberButtonClick('1')}>
-            <Icon icon="material-symbols:counter-1" height={45} /></button>
-          <button onClick={() => numberButtonClick('0')}>
-            <Icon icon="material-symbols:counter-0" height={45} /></button>
-          <button onClick={() => handlerClear()}>
-            <Icon icon="material-symbols:copyright-outline-rounded" height={45} /></button>
-          <button onClick={() => numberButtonClick('.')}> 
-            <Icon icon="tabler:point-filled" height={45} /></button>
+          <input
+            value={
+              !operator
+                ? currentValue
+                : prevValue
+                  ? `${prevValue} ${operator} ${currentValue}`
+                  : currentValue
+            }
+            readOnly
+            onKeyDown={handleKeyDown}
+            className="screen"
+          />
+          <div className="button-container">
+            <div className="number-buttons">
+              <button onClick={() => numberButtonClick('7')} className="button number">
+                <Icon icon="material-symbols:counter-7" height={45} />
+              </button>
+              <button onClick={() => numberButtonClick('8')} className="button number">
+                <Icon icon="material-symbols:counter-8" height={45} />
+              </button>
+              <button onClick={() => numberButtonClick('9')} className="button number">
+                <Icon icon="material-symbols:counter-9" height={45} />
+              </button>
+              <button onClick={() => numberButtonClick('4')} className="button number">
+                <Icon icon="material-symbols:counter-4" height={45} />
+              </button>
+              <button onClick={() => numberButtonClick('5')} className="button number">
+                <Icon icon="material-symbols:counter-5" height={45} />
+              </button>
+              <button onClick={() => numberButtonClick('6')} className="button number">
+                <Icon icon="material-symbols:counter-6" height={45} />
+              </button>
+              <button onClick={() => numberButtonClick('1')} className="button number">
+                <Icon icon="material-symbols:counter-1" height={45} />
+              </button>
+              <button onClick={() => numberButtonClick('2')} className="button number">
+                <Icon icon="material-symbols:counter-2" height={45} />
+              </button>
+              <button onClick={() => numberButtonClick('3')} className="button number">
+                <Icon icon="material-symbols:counter-3" height={45} />
+              </button>
+              <button onClick={() => handlerClear()} className="button clear">
+                <Icon icon="material-symbols:copyright-outline-rounded" height={45} />
+              </button>
+              <button onClick={() => numberButtonClick('0')} className="button number">
+                <Icon icon="material-symbols:counter-0" height={45} />
+              </button>
+              <button onClick={() => numberButtonClick('.')} className="button number">
+                <Icon icon="tabler:point-filled" height={45} />
+              </button>
+            </div>
+            <div className="operator-buttons">
+              <button onClick={() => operatorButtonClick('+')} className="button operator">
+                <Icon icon="whh:calcplus" height={45} />
+              </button>
+              <button onClick={() => operatorButtonClick('-')} className="button operator">
+                <Icon icon="whh:calcminus" height={45} />
+              </button>
+              <button onClick={() => operatorButtonClick('*')} className="button operator">
+                <Icon icon="whh:calcmultiply" height={45} />
+              </button>
+              <button onClick={() => operatorButtonClick('/')} className="button operator">
+                <Icon icon="whh:calcdivide" height={45} />
+              </button>
+              <button onClick={() => handleEqualsClick()} className="button equal">
+                <Icon icon="whh:calcequals" height={45} />
+              </button>
+              <button onClick={() => operatorButtonClick('%')} className="button percentage">
+                <Icon icon="fa-solid:percentage" height={45} />
+              </button>
+            </div>
+          </div>
         </div>
       </div>
-
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
